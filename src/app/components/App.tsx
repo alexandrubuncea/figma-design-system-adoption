8953e7df672418cc033017751600662acae0c320
Alexandru Buncea <alexandrubuncea@gmail.com>
2023-12-23 18:51:31 +0200

    Added UI, Tailwind and Calculations

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -1,45 +1,188 @@
-import React from 'react';
-import logo from '../assets/logo.svg';
+import React, { useEffect } from 'react';
 import '../styles/ui.css';
 
 function App() {
-  const textbox = React.useRef<HTMLInputElement>(undefined);
+  const [matchingComponentsCount, setMatchingComponentsCount] = React.useState(0);
+  const [matchingFillsCount, setMatchingFillsCount] = React.useState(0);
+  const [matchingTextStylesCount, setMatchingTextStylesCount] = React.useState(0);
+  const [allTextNodesCount, setAllTextNodesCount] = React.useState(0);
+  const [allFillsCount, setAllFillsCount] = React.useState(0);
+  const [isLibrariesFetched, setIsLibraryFetched] = React.useState(false);
+  const [isAnalysing, setIsAnalysing] = React.useState(false);
+  const textPercentage = ((matchingTextStylesCount / allTextNodesCount) * 100).toFixed(0);
+  const fillsPercentage = ((matchingFillsCount / allFillsCount) * 100).toFixed(0);
 
-  const countRef = React.useCallback((element: HTMLInputElement) => {
-    if (element) element.value = '5';
-    textbox.current = element;
-  }, []);
+  useEffect(() => {
+    // Listen for messages from the plugin
+    window.addEventListener('message', handleMessage);
 
-  const onCreate = () => {
-    const count = parseInt(textbox.current.value, 10);
-    parent.postMessage({ pluginMessage: { type: 'create-rectangles', count } }, '*');
-  };
-
-  const onCancel = () => {
-    parent.postMessage({ pluginMessage: { type: 'cancel' } }, '*');
-  };
-
-  React.useEffect(() => {
-    // This is how we read messages sent from the plugin controller
-    window.onmessage = (event) => {
-      const { type, message } = event.data.pluginMessage;
-      if (type === 'create-rectangles') {
-        console.log(`Figma Says: ${message}`);
-      }
+    // Clean up the event listener
+    return () => {
+      window.removeEventListener('message', handleMessage);
     };
   }, []);
 
+  const handleMessage = (event: MessageEvent) => {
+    const {
+      type,
+      matchingComponentsCount,
+      matchingFillsCount,
+      matchingTextStylesCount,
+      allTextNodesCount,
+      allFillsCount,
+    } = event.data.pluginMessage;
+
+    if (type === 'update-counts') {
+      // Handle the score and counts here
+      // For example, you might set state variables or update the UI directly
+
+      setMatchingComponentsCount(matchingComponentsCount);
+      setMatchingFillsCount(matchingFillsCount);
+      setMatchingTextStylesCount(matchingTextStylesCount);
+      setAllTextNodesCount(allTextNodesCount);
+      setAllFillsCount(allFillsCount);
+      setIsAnalysing(false);
+    } else if (type === 'fetched') {
+      setIsLibraryFetched(true);
+      console.log('Libraries loaded');
+    }
+  };
+
+  const handleAnalyseClick = () => {
+    setIsAnalysing(true);
+    parent.postMessage({ pluginMessage: { type: 'analyse' } }, '*');
+    console.log('Analyse clicked');
+  };
+
   return (
-    <div>
-      <img src={logo} />
-      <h2>Rectangle Creator</h2>
-      <p>
-        Count: <input ref={countRef} />
-      </p>
-      <button id="create" onClick={onCreate}>
-        Create
-      </button>
-      <button onClick={onCancel}>Cancel</button>
+    <div className="p-4 flex flex-col gap-4 h-full justify-between">
+      {isLibrariesFetched ? (
+        <div className="flex flex-col gap-4">
+          <div className=" flex flex-col gap-1">
+            <h1 className="text-2xl font-bold">Design System Adoption</h1>
+            <p className="text-sm text-neutral-700">
+              Analyse your design to see how many components, text styles and fill styles are used from your design
+              system.
+            </p>
+          </div>
+          <div className="flex flex-col divide-y border-2 border-neutral-100 rounded-lg">
+            <div className="px-4 py-2 w-full flex flex-row justify-between items-center ">
+              <div>
+                <p className="text-lg font-bold">Component adoption</p>
+                {isAnalysing ? (
+                  <div className="animate-pulse h-5 bg-neutral-100 rounded w-2/4"></div>
+                ) : (
+                  <p className="text-sm text-neutral-700">{matchingComponentsCount} nodes</p>
+                )}
+              </div>
+            </div>
+            <div className="px-4 py-2 w-full flex flex-row justify-between items-center">
+              <div>
+                <p className="text-lg font-bold">Fill styles adoption</p>
+                {isAnalysing ? (
+                  <div className="animate-pulse h-5 bg-neutral-100 rounded w-2/4"></div>
+                ) : (
+                  <p className="text-sm text-neutral-700">
+                    {matchingFillsCount} / {allFillsCount} of fill nodes
+                  </p>
+                )}
+              </div>
+              <div>
+                {isAnalysing ? (
+                  <div className="animate-pulse h-8 bg-neutral-100 rounded w-16"></div>
+                ) : (
+                  <p className="text-2xl font-bold">{fillsPercentage}%</p>
+                )}
+              </div>
+            </div>
+            <div className="px-4 py-2 w-full flex flex-row justify-between items-center">
+              <div>
+                <p className="text-lg font-bold">Text styles adoption</p>
+                {isAnalysing ? (
+                  <div className="animate-pulse h-5 bg-neutral-100 rounded w-2/4"></div>
+                ) : (
+                  <p className="text-sm text-neutral-700">
+                    {matchingTextStylesCount} / {allTextNodesCount} of text nodes
+                  </p>
+                )}
+              </div>
+              <div>
+                {isAnalysing ? (
+                  <div className="animate-pulse h-8 bg-neutral-100 rounded w-16"></div>
+                ) : (
+                  <p className="text-2xl font-bold">{textPercentage}%</p>
+                )}
+              </div>
+            </div>
+          </div>
+          <button
+            type="button"
+            className={`px-4 py-3 flex flex-row items-center justify-center w-full rounded-full font-bold text-white ${
+              isAnalysing ? 'bg-purple-900' : 'bg-purple-600 hover:bg-purple-700'
+            } cursor-pointer relative`}
+            onClick={handleAnalyseClick}
+            disabled={isAnalysing}
+          >
+            {isAnalysing ? (
+              <div className="flex items-center">
+                <svg
+                  className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+                  <path
+                    className="opacity-75"
+                    fill="currentColor"
+                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                  ></path>
+                </svg>
+                Processing...
+              </div>
+            ) : (
+              'Analyse design'
+            )}
+          </button>
+        </div>
+      ) : (
+        <div className="flex flex-col gap-4">
+          <div className="animate-pulse flex flex-col gap-4">
+            <div className="flex flex-col gap-1">
+              <div className="h-6 bg-neutral-100 rounded w-1/2"></div>
+              <div className="h-4 bg-neutral-100 rounded w-full"></div>
+              <div className="h-4 bg-neutral-100 rounded w-3/4"></div>
+            </div>
+            <div className="flex flex-col divide-y border-2 border-neutral-100 rounded-lg">
+              <div className="px-4 py-2 w-full flex flex-row justify-between items-center h-16">
+                <div>
+                  <div className="h-4 bg-neutral-100 rounded w-1/2"></div>
+                  <div className="h-3 bg-neutral-100 rounded w-1/4"></div>
+                </div>
+              </div>
+              <div className="px-4 py-2 w-full flex flex-row justify-between items-center h-16">
+                <div>
+                  <div className="h-4 bg-neutral-100 rounded w-1/2"></div>
+                  <div className="h-3 bg-neutral-100 rounded w-1/4"></div>
+                </div>
+                <div>
+                  <div className="h-6 bg-neutral-100 rounded w-1/4"></div>
+                </div>
+              </div>
+              <div className="px-4 py-2 w-full flex flex-row justify-between items-center h-16">
+                <div>
+                  <div className="h-4 bg-neutral-100 rounded w-1/2"></div>
+                  <div className="h-3 bg-neutral-100 rounded w-1/4"></div>
+                </div>
+                <div>
+                  <div className="h-6 bg-neutral-100 rounded w-1/4"></div>
+                </div>
+              </div>
+            </div>
+            <div className="px-4 py-2 w-full rounded-full bg-neutral-100 h-12 cursor-pointer"></div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
--- src/plugin/controller.ts
+++ src/plugin/controller.ts
@@ -1,26 +1,374 @@
-figma.showUI(__html__);
+figma.showUI(__html__, {
+  title: 'System Score',
+  themeColors: true,
+  width: 400,
+  height: 600,
+});
 
-figma.ui.onmessage = (msg) => {
-  if (msg.type === 'create-rectangles') {
-    const nodes = [];
+import config from './config-keys.ts';
 
-    for (let i = 0; i < msg.count; i++) {
-      const rect = figma.createRectangle();
-      rect.x = i * 150;
-      rect.fills = [{ type: 'SOLID', color: { r: 1, g: 0.5, b: 0 } }];
-      figma.currentPage.appendChild(rect);
-      nodes.push(rect);
-    }
+// Relaunch
+figma.root.setRelaunchData({ open: '' });
 
-    figma.currentPage.selection = nodes;
-    figma.viewport.scrollAndZoomIntoView(nodes);
+const figmaFileId = config.figmaFileId;
+const foundationsFileId = config.foundationsFileId;
+const teamId = config.teamId;
+const figmaApiToken = config.figmaApiToken;
 
-    // This is how figma responds back to the ui
-    figma.ui.postMessage({
-      type: 'create-rectangles',
-      message: `Created ${msg.count} Rectangles`,
-    });
+let components;
+let styles;
+let libraries = [];
+
+// Fetch components
+
+async function fetchAllComponents(figmaFileId: string, url: string, allComponents: any[] = []) {
+  const response = await fetch(url, {
+    method: 'GET',
+    headers: {
+      'X-Figma-Token': figmaApiToken,
+    },
+  });
+  const json = await response.json();
+
+  console.log('Components:', json);
+
+  // Check if the response has a 'meta' property and 'components' within it
+  if (json.meta && json.meta.components) {
+    console.log('Accumulating Components:', json.meta.components);
+    allComponents = allComponents.concat(json.meta.components);
+    console.log('Total Components Accumulated:', allComponents.length);
+  } else {
+    console.log('No components found in the current response.');
   }
 
-  figma.closePlugin();
+  return allComponents;
+}
+
+async function fetchAllStyles(foundationsFileId: string, url: string, allStyles: any[] = []) {
+  console.log('Fetching from URL:', url);
+
+  const response = await fetch(url, {
+    method: 'GET',
+    headers: {
+      'X-Figma-Token': figmaApiToken,
+    },
+  });
+
+  if (!response.ok) {
+    console.error('Error fetching data:', response.statusText);
+    return;
+  }
+
+  const json = await response.json();
+
+  console.log('Styles:', json);
+
+  // Check if the response has a 'meta' property and 'styles' within it
+  if (json.meta && json.meta.styles) {
+    console.log('Accumulating Styles:', json.meta.styles);
+    allStyles = allStyles.concat(json.meta.styles);
+    console.log('Total Styles Accumulated:', allStyles.length);
+  } else {
+    console.log('No styles found in the current response.');
+  }
+
+  return allStyles;
+}
+
+async function fetchAllComponentsAndLayers(figmaFileId: string) {
+  const url = `https://api.figma.com/v1/files/${figmaFileId}`;
+
+  const response = await fetch(url, {
+    method: 'GET',
+    headers: {
+      'X-Figma-Token': figmaApiToken,
+    },
+  });
+
+  const json = await response.json();
+
+  console.log('Components and Layers:', json);
+
+  return json;
+}
+
+fetchAllComponentsAndLayers(figmaFileId);
+
+// Fetch components and styles
+Promise.all([
+  fetchAllComponents(figmaFileId, `https://api.figma.com/v1/files/${figmaFileId}/components?page_size=100000`),
+  fetchAllStyles(foundationsFileId, `https://api.figma.com/v1/files/${foundationsFileId}/styles?page_size=100000`),
+])
+  .then(([fetchedComponents, fetchedStyles]) => {
+    // Update the components and styles
+    components = fetchedComponents;
+    styles = fetchedStyles;
+
+    // Add the fetched components and styles to the libraries
+    libraries.push(fetchedComponents, fetchedStyles);
+
+    // Log a message to the console
+    console.log('About to send fetched message');
+
+    // Send a message to the UI
+    figma.ui.postMessage({ type: 'fetched', message: 'Components and styles fetched successfully.' });
+
+    return { components, styles, libraries };
+  })
+  .catch((error) => {
+    console.error('Error fetching components and styles:', error);
+
+    // Send an error message to the UI
+    figma.ui.postMessage({ type: 'error', message: 'Error fetching components and styles.' });
+  });
+
+// Getting all the nodes within a selection
+function getNodes(nodes: readonly SceneNode[]): SceneNode[] {
+  const allNodes: SceneNode[] = [];
+
+  for (const node of nodes) {
+    if (node.visible) {
+      allNodes.push(node);
+      if ('children' in node) {
+        const childNodes = getNodes(node.children);
+        allNodes.push(...childNodes);
+      }
+    }
+  }
+
+  return allNodes;
+}
+
+// Get node data
+// function getNodeData(nodes) {
+//   return nodes.map((node) => {
+//     let fillStyleKey = null;
+//     let textStyleKey = null;
+//     if (node.fillStyleId) {
+//       const fillStyle = figma.getStyleById(node.fillStyleId);
+//       if (fillStyle) {
+//         fillStyleKey = fillStyle.key;
+//       }
+//     }
+//     if (node.type === 'TEXT') {
+//       const textStyle = figma.getStyleById(node.textStyleId);
+//       if (textStyle) {
+//         textStyleKey = textStyle.key;
+//       }
+//     }
+//     return {
+//       name: node.name,
+//       id: node.id,
+//       type: node.type,
+//       visible: node.visible,
+//       hasFill: node.fills.length > 0,
+//       fillStyleId: node.fillStyleId,
+//       strokeStyleId: node.strokeStyleId,
+//       textStyleId: node.textStyleId,
+//       mainComponent: node.mainComponent,
+//       parent: node.parent,
+//       mainComponentKey: node.mainComponent ? node.mainComponent.key : null,
+//       fillStyleKey: fillStyleKey,
+//       textStyleKey: textStyleKey,
+//     };
+//   });
+// }
+
+function getNodeData(nodes, parentMainComponentKey = null) {
+  return nodes.flatMap((node) => {
+    let fillStyleKey = null;
+    let textStyleKey = null;
+
+    if (node.fillStyleId) {
+      const fillStyle = figma.getStyleById(node.fillStyleId);
+      if (fillStyle) {
+        fillStyleKey = fillStyle.key;
+      }
+    }
+
+    if (node.type === 'TEXT') {
+      const textStyle = figma.getStyleById(node.textStyleId);
+      if (textStyle) {
+        textStyleKey = textStyle.key;
+      }
+    }
+
+    let mainComponentKey = node.mainComponent ? node.mainComponent.key : null;
+    let isNestedInInstance = false; // New variable to track if node is nested in an instance
+
+    // If the node is a child of an instance and doesn't have a different main component, it comes from the library
+    if (node.parent && node.parent.type === 'INSTANCE' && mainComponentKey === null) {
+      mainComponentKey = node.parent.mainComponent ? node.parent.mainComponent.key : null;
+      isNestedInInstance = true;
+    }
+
+    const nodeData = {
+      name: node.name,
+      id: node.id,
+      type: node.type,
+      visible: node.visible,
+      hasFill: node.fills.length > 0,
+      fillStyleId: node.fillStyleId,
+      strokeStyleId: node.strokeStyleId,
+      textStyleId: node.textStyleId,
+      mainComponent: node.mainComponent,
+      parent: node.parent,
+      parentMainComponentKey: node.parent && node.parent.mainComponent ? node.parent.mainComponent.key : null,
+      mainComponentKey: mainComponentKey,
+      fillStyleKey: fillStyleKey,
+      textStyleKey: textStyleKey,
+      isNestedInInstance: isNestedInInstance,
+    };
+
+    return [nodeData];
+  });
+}
+
+// Function to check if keys of the node match with keys from the component or style library
+function checkKeys(nodeData, libraries) {
+  const keys = libraries.flatMap((library) => library.map((item) => item.key));
+  const nodeKeys = nodeData.flatMap((item) => [item.mainComponentKey, item.fillStyleKey, item.textStyleKey]);
+
+  // Initialize objects to store matching keys for each type
+  const matchingKeys = {
+    mainComponentKeys: [],
+    fillStyleKeys: [],
+    textStyleKeys: [],
+  };
+
+  // For each type of key, check if it exists in the list of keys and store matching keys
+  nodeKeys.forEach((key) => {
+    if (keys.includes(key)) {
+      if (nodeData.some((item) => item.mainComponentKey === key)) {
+        matchingKeys.mainComponentKeys.push(key);
+      }
+      if (nodeData.some((item) => item.fillStyleKey === key)) {
+        matchingKeys.fillStyleKeys.push(key);
+      }
+      if (nodeData.some((item) => item.textStyleKey === key)) {
+        matchingKeys.textStyleKeys.push(key);
+      }
+    }
+  });
+
+  console.log('Keys:', keys);
+  console.log('Node Keys:', nodeKeys);
+  console.log('Matching Main Component Keys:', matchingKeys.mainComponentKeys);
+  console.log('Matching Fill Style Keys:', matchingKeys.fillStyleKeys);
+  console.log('Matching Text Style Keys:', matchingKeys.textStyleKeys);
+
+  return matchingKeys;
+}
+
+async function checkSelection() {
+  const selection = figma.currentPage.selection;
+  const allNodes = getNodes(selection);
+
+  console.log('Selection:', selection);
+
+  if (allNodes.length > 0) {
+    // Replace the mapping logic with a call to getNodeData
+    const nodeData = getNodeData(allNodes);
+
+    console.log('Node Data:', nodeData); // Log the node data
+    // // Send the list of nodes to the UI
+    // figma.ui.postMessage({ type: 'nodes', nodes: nodeData });
+    // figma.ui.postMessage({ type: 'selection-changed' });
+
+    console.log('Components:', components);
+    console.log('Styles:', styles);
+
+    console.log('About to call checkKeys');
+
+    // Check if keys of the node match with keys from the fetched components and styles
+    const matchingKeys = checkKeys(nodeData, [components, styles]);
+
+    console.log('Matching Main Component Keys:', matchingKeys.mainComponentKeys.length);
+    console.log('Matching Fill Style Keys:', matchingKeys.fillStyleKeys.length);
+    console.log('Matching Text Style Keys:', matchingKeys.textStyleKeys.length);
+
+    // Send the matching keys numbers to the ui
+    figma.ui.postMessage({
+      type: 'update-counts',
+      matchingComponentsCount: matchingKeys.mainComponentKeys.length,
+      matchingFillsCount: matchingKeys.fillStyleKeys.length,
+      matchingTextStylesCount: matchingKeys.textStyleKeys.length,
+      allTextNodesCount: nodeData.filter((node) => node.type === 'TEXT').length,
+      allFillsCount: nodeData.filter((node) => node.hasFill).length,
+    });
+
+    return nodeData;
+  } else {
+    // Send the 'nodes-not-found' message to the UI
+    figma.ui.postMessage({ type: 'nodes-not-found' });
+  }
+}
+
+figma.ui.onmessage = (message) => {
+  if (message.type === 'analyse') {
+    checkSelection();
+  }
 };
+// figma.on('selectionchange', async () => {
+//   // Get all nodes in the current selection
+//   const allNodes = figma.currentPage.selection.flatMap((node) => [node, ...node.findAll()]);
+
+//   // Get all visible nodes
+//   const visibleNodes = figma.currentPage.selection.filter((node) => node.visible);
+//   const visibleFillStyles = figma.currentPage.selection.filter((node) => node.fillStyleId !== '');
+//   const visibleStrokeStyles = figma.currentPage.selection.filter((node) => node.strokeStyleId !== '');
+//   const visibleTextStyles = figma.currentPage.selection.filter((node) => node.textStyleId !== '');
+
+//   let fromLibraryCount = 0;
+//   let fromOtherLibrariesCount = 0;
+//   let textNodesWithMatchingStyleCount = 0;
+
+//   // Iterate over all nodes
+//   for (const node of visibleNodes) {
+//     // Get the source of the node
+//     const nodeSource = node.mainComponent;
+
+//     // Get the key of the node source
+//     const nodeSourceKey = nodeSource ? nodeSource.key : null;
+
+//     // Count how many nodes come from the library
+//     if (nodeSourceKey) {
+//       const nodesFromLibrary = components.filter((component) => component.key === nodeSourceKey);
+//       const nodesFromLibraryCount = nodesFromLibrary.length;
+
+//       if (nodesFromLibraryCount > 0) {
+//         fromLibraryCount++;
+//       } else {
+//         fromOtherLibrariesCount++;
+//       }
+//     }
+
+//     // // Get all the used styles in all the visible nodes
+//     // const nodeFillStyleId = node.fillStyleId;
+//     // const nodeStrokeStyleId = node.strokeStyleId;
+//     // const nodeTextStyleId = node.textStyleId;
+
+//     // // Check the keys of the used styles against the keys of the styles in the library
+//     // const matchingFillStyles = styles.filter((style) => style.key === nodeFillStyleId);
+//   }
+
+//   console.log('From the library: ' + fromLibraryCount);
+//   console.log('From other libraries: ' + fromOtherLibrariesCount);
+//   console.log('Text nodes with matching style: ' + textNodesWithMatchingStyleCount);
+//   console.log('Total nodes: ' + visibleNodes.length);
+
+//   // Calculate the score
+//   let score = 0;
+//   if (allNodes.length > 0) {
+//     score = (fromLibraryCount / allNodes.length) * 100;
+//   }
+//   console.log('Score: ' + score.toFixed(2) + '%');
+
+//   // Send a message to the UI with the score and counts
+//   figma.ui.postMessage({
+//     type: 'update-counts',
+//     score: score.toFixed(2),
+//     fromLibraryCount,
+//     fromOtherLibrariesCount,
+//   });
+// });
